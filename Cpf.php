e39fa6c5fa5f6bbf1f862b35b3a0f0a924edfbd9
Malgany <yopsadida@gmail.com>
2020-07-11 22:31:28 -0300

    Create Cpf.php

---
+++ Cpf.php
@@ -0,0 +1,32 @@
+<?php
+
+class Cpf
+{
+    public function cpfRandom(string $mascara = "1") : string
+    {
+        $n1 = 6;//rand(0, 9);
+        $n2 = 8;//rand(0, 9);
+        $n3 = 0;//rand(0, 9);
+        $n4 = 2;//rand(0, 9);
+        $n5 = 1;//rand(0, 9);
+        $n6 = 3;//rand(0, 9);
+        $n7 = 7;//rand(0, 9);
+        $n8 = 4;//rand(0, 9);
+        $n9 = 6;//rand(0, 9);
+
+        $dv1 = $n9 * 2 + $n8 * 3 + $n7 * 4 + $n6 * 5 + $n5 * 6 + $n4 * 7 + $n3 * 8 + $n2 * 9 + $n1 * 10;
+        $dv1 = 11 - ($dv1 % 11);
+
+        $dv2 = $dv1 * 2 + $n9 * 3 + $n8 * 4 + $n7 * 5 + $n6 * 6 + $n5 * 7 + $n4 * 8 + $n3 * 9 + $n2 * 10 + $n1 * 11;
+        $dv2 = 11 - ($dv2 % 11);
+
+        $retorno = $n1 . $n2 . $n3 . $n4 . $n5 . $n6 . $n7 . $n8 . $n9 . $dv1 . $dv2;
+
+        if($mascara == "1") {
+            $retorno = $n1 . $n2 . $n3 . "." . $n4 . $n5 . $n6 . "." . $n7 . $n8 . $n9 . "-" . $dv1 . $dv2;
+        }
+
+        return $retorno;
+    }
+
+}
